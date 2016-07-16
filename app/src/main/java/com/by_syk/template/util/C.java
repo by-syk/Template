b87b8f23289bfc1ac05046c70d558fed584e0539
By_syk <By_syk@163.com>
2016-07-16 09:40:35 +0800

    v1.0.0

---
+++ app/src/main/java/com/by_syk/template/util/C.java
@@ -0,0 +1,12 @@
+package com.by_syk.template.util;
+
+import android.os.Build;
+
+/**
+ * Created by By_syk on 2016-07-16.
+ */
+public class C {
+    public static final int SDK = Build.VERSION.SDK_INT;
+
+    public static final String LOG_TAG = "TEMPLATE";
+}
